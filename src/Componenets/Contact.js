c7572bf8bd619f70e5ae5df675294e202ee2bfd6
RAMESH <rmshvalle@gmail.com>
2025-10-22 23:00:50 +0530

    Fix folder name: track Components, remove Componenets

--- src/Componenets/Contact.js
+++
@@ -1,7 +0,0 @@
-const Contact = () => {
-    return (
-        <h1 className = "text-center font-bold text-2xl text-blue-600 underline py-3"> No contact number available !!! </h1>
-    );
-}; 
-
-export default Contact;
